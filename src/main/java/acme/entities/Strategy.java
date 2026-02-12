93f8388445b91be2ec7461a09794a8a9c6b97ba2
Sparky062 <victormt001@gmail.com>
2026-02-12 20:24:20 +0100

    all classes, except for Temporal constraints and derived atributes

---
+++ src/main/java/acme/entities/Strategy.java
@@ -0,0 +1,82 @@
+
+package acme.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Moment;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoment.Constraint;
+import acme.client.components.validation.ValidScore;
+import acme.client.components.validation.ValidUrl;
+import acme.realms.Fundraiser;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Strategy extends AbstractEntity {
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	//@ValidTicker
+	@Column(unique = true)
+	private String				ticker;
+
+	@Mandatory
+	//@ValidHeader
+	@Column
+	private String				name;
+
+	@Mandatory
+	//@ValidText
+	@Column
+	private String				description;
+
+	@Mandatory
+	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
+	private Moment				startMoment;
+
+	@Mandatory
+	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
+	private Moment				endMoment;
+
+	@Optional
+	@ValidUrl
+	@Column
+	private String				moreInfo;
+
+
+	@Valid
+	@Transient
+	private Double monthsActive() {
+		return null;
+	};
+
+
+	@Mandatory
+	@ValidScore
+	@Transient
+	private Double		expectedPercentage;
+
+	@Mandatory
+	@Valid
+	@Column
+	private Boolean		draftMode;
+
+	@Mandatory
+	@Valid
+	@ManyToOne
+	private Fundraiser	fundraiser;
+}
