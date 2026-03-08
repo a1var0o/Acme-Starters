2409fa0a5d8e8ef1dd534ae4845186f194b11538
León Martín <alemarlam@alum.us.es>
2026-03-08 22:32:01 +0100

    add button to see sponsor profiles

--- src/main/java/acme/features/any/sponsorship/AnySponsorshipShowService.java
+++ src/main/java/acme/features/any/sponsorship/AnySponsorshipShowService.java
@@ -1,17 +1,13 @@
 
 package acme.features.any.sponsorship;
 
-import java.util.Collection;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.client.components.models.Tuple;
 import acme.client.components.principals.Any;
-import acme.client.components.views.SelectChoices;
 import acme.client.services.AbstractService;
 import acme.entities.Sponsorship;
-import acme.realms.Sponsor;
 
 @Service
 public class AnySponsorshipShowService extends AbstractService<Any, Sponsorship> {
@@ -45,16 +41,11 @@ public class AnySponsorshipShowService extends AbstractService<Any, Sponsorship>
 
 	@Override
 	public void unbind() {
-		Collection<Sponsor> sponsors;
-		SelectChoices choices;
 		Tuple tuple;
 
-		sponsors = this.repository.findAllSponsors();
-		choices = SelectChoices.from(sponsors, "identity.fullName", this.sponsorship.getSponsor());
-
 		tuple = super.unbindObject(this.sponsorship, "ticker", "name", "startMoment", "endMoment", "moreInfo", "description");
-		tuple.put("sponsor", choices.getSelected().getKey());
-		tuple.put("sponsors", choices);
+		tuple.put("sponsor", this.sponsorship.getSponsor().getIdentity().getFullName());
+		tuple.put("sponsorId", this.sponsorship.getSponsor().getId());
 	}
 
 }
