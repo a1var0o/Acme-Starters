8b9e3714a09fe26cc64b1b58fb96d1ad8f47f603
alvaro <alvvilale@alum.us.es>
2026-04-30 19:31:22 +0200

    feat: add manager update project

---
+++ src/main/java/acme/features/manager/project/ManagerProjectRepository.java
@@ -0,0 +1,16 @@
+
+package acme.features.manager.project;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.Project;
+
+@Repository
+public interface ManagerProjectRepository extends AbstractRepository {
+
+	@Query("select p from Project p where p.id = :id")
+	Project findProject(int id);
+
+}
